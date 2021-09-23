c8b68d80d30b333b14f2e78b10c28a13294d5c3d
alisamaximova <maximova-alisa@mail.ru>
2021-09-23 20:44:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,34 +4,34 @@
 
 int main()
 {
-	char numeric;
+    char numeric;
 	while((numeric = getchar()) != EOF // setting a condition for the entered variable
 	      && numeric != '\n' 
 	      && numeric != '\0')
-	{
-		switch(numeric) 
+    {
+        switch(numeric) 
         {
-        	case ' ':
-        	case '-':
-           		break; // ignoring spaces and the minus sign
-        	case '0':
-        	case '1':
-        	case '2':
-        	case '3':
-        	case '4':
-        	case '5':
-        	case '6':
-       	 	case '7':
-        	case '8':
-        	case '9':
-				printf("%c", numeric);
-            	break;
-        	case '.':
-            	printf(" | "); // replacing characters
-            	break; 
+            case ' ':
+            case '-':
+                break; // ignoring spaces and the minus sign
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                printf("%c", numeric);
+                break;
+            case '.':
+                printf(" | "); // replacing characters
+                break; 
         	default:
-            	printf("\n\nError\n\n"); // informing about an error
-            	exit(-1);
+                printf("\n\nError\n\n"); // informing about an error
+                exit(-1);
 		}
 	}
 	printf("\n");
