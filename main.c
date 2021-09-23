360c2b1431a5e4fcb46af37df8c76246ba2cd8a5
alisamaximova <maximova-alisa@mail.ru>
2021-09-23 17:35:59 +0300

    Update main.c
    Formatting the code

--- main.c
+++ main.c
@@ -9,29 +9,29 @@ int main()
 	      && numeric != '\n' 
 	      && numeric != '\0')
 	{
-        	switch(numeric) 
-        	{
-        		case ' ':
-        		case '-':
-           			break; // ignoring spaces and the minus sign
-        		case '0':
-        		case '1':
-        		case '2':
-        		case '3':
-        		case '4':
-        		case '5':
-        		case '6':
-       	 		case '7':
-        		case '8':
-        		case '9':
-            			printf("%c", numeric);
-            			break;
-        		case '.':
-            			printf(" | "); // replacing characters
-            			break; 
-        		default:
-            			printf("\n\nError\n\n"); // informing about an error
-            			exit(-1);
+		switch(numeric) 
+        {
+        	case ' ':
+        	case '-':
+           		break; // ignoring spaces and the minus sign
+        	case '0':
+        	case '1':
+        	case '2':
+        	case '3':
+        	case '4':
+        	case '5':
+        	case '6':
+       	 	case '7':
+        	case '8':
+        	case '9':
+				printf("%c", numeric);
+            	break;
+        	case '.':
+            	printf(" | "); // replacing characters
+            	break; 
+        	default:
+            	printf("\n\nError\n\n"); // informing about an error
+            	exit(-1);
 		}
 	}
 	printf("\n");
